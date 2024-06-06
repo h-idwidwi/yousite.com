7a4dca05659da2bb71d4a794d2ce015e97b3e601
Ульяна <131362344+h-idwidwi@users.noreply.github.com>
2024-06-06 19:15:37 +0500

    .

--- routes/api.php
+++ routes/api.php
@@ -57,16 +57,18 @@ Route::middleware(['CheckPermission', 'MakeLog'])->group(function () {
 });
 
 Route::prefix('auth')->group(function() {
-    Route::post('login', [AuthController::class, 'login'])->name('login');
-    Route::post('/2fa/verify', [AuthController::class, 'confirm2FACode'])->middleware('IsExpiry');
-    Route::post('/2fa/resend', [AuthController::class, 'resendCode']);
-    Route::middleware('AuthCheck')->group(function (){
-        Route::post('register', [AuthController::class, 'register'])->name('register');
-    });
-    Route::middleware(['auth:api', 'IsExpiry'])->group(function () {
-        Route::get('me', [UserController::class, 'me'])->name('me');
-        Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-        Route::post('logout_all', [AuthController::class, 'logout_all'])->name('logout_all');
-        Route::get('tokens', [UserController::class, 'tokens'])->name('tokens');
+    Route::middleware('MakeLog')->group(function (){
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('/2fa/verify', [AuthController::class, 'confirm2FACode'])->middleware('IsExpiry');
+        Route::post('/2fa/resend', [AuthController::class, 'resendCode']);
+        Route::middleware('AuthCheck')->group(function (){
+            Route::post('register', [AuthController::class, 'register'])->name('register');
+        });
+        Route::middleware(['auth:api', 'IsExpiry'])->group(function () {
+            Route::get('me', [UserController::class, 'me'])->name('me');
+            Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+            Route::post('logout_all', [AuthController::class, 'logout_all'])->name('logout_all');
+            Route::get('tokens', [UserController::class, 'tokens'])->name('tokens');
+        });
     });
 });
